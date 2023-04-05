7585e82da8285aee85b3ef6269cbf18258b11af4
William Lathan <wlatha2@hotmail.com>
2023-04-05 00:58:43 -0500

    Slight change of comment wording

--- OldRedditURLRedirectBookmarklet.js
+++ OldRedditURLRedirectBookmarklet.js
@@ -1,3 +1,3 @@
-//I prefer the old reddit format, so I made this bookmarklet to let me quickly switch any tab I view to the older reddit view
+//I prefer the old reddit format, so I made this bookmarklet for quick redirects
 javascript:(()=>{let currentURL = window.location.href;let newURL = currentURL.split("https://www.");
 window.location.replace("https://old." + newURL[1]);})();
